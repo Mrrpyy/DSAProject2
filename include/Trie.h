40b37ebea104ef8885bf523ebc9d33a447a19130
Guilherme Carvalheira <guilhermecarvalheiraalz@gmail.com>
2026-07-09 18:18:32 -0400

    Implement trie search and autocomplete

--- include/Trie.h
+++ include/Trie.h
@@ -3,4 +3,19 @@
 #include <vector>
 #include "Movie.h"
 #include "TrieNode.h"
-class Trie{public: Trie(); void insert(const Movie&); bool search(const std::string&); std::vector<Movie> autocomplete(const std::string&); private: TrieNode* root;};
+
+class Trie {
+public:
+    Trie();
+    ~Trie();
+
+    void insert(const Movie& movie);
+    bool search(const std::string& title) const;
+    std::vector<Movie> autocomplete(const std::string& prefix) const;
+
+private:
+    TrieNode* root;
+
+    void collectMovies(const TrieNode* node, std::vector<Movie>& results) const;
+    static void deleteNodes(TrieNode* node);
+};
--- include/TrieNode.h
+++ include/TrieNode.h
@@ -1,3 +1,10 @@
 #pragma once
 #include <unordered_map>
-struct TrieNode{bool isEndOfWord=false; std::unordered_map<char,TrieNode*> children;};
+
+#include "Movie.h"
+
+struct TrieNode {
+    bool isEndOfWord = false;
+    std::unordered_map<char, TrieNode*> children;
+    Movie movie;
+};
